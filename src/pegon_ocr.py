0ae611da9bff4eee3777fa5702743a38e9721318
Andre Septiano <andreseptiano169@gmail.com>
2023-06-02 23:57:30 +0700

    Refactor original code from Hussein's repository

--- src/pegon_ocr.py
+++ src/pegon_ocr.py
@@ -17,7 +17,7 @@ tokens_to_unknown = ['[CALLIGRAPHY]',
                      '[VERT]',
                     ]
 
-model_name = '2L_NN.sav'
+model_name = '1L_NN.sav'
 def load_model():
     location = 'models'
     if os.path.exists(location):
@@ -29,7 +29,7 @@ def run2(obj):
     model = load_model()
     # For each word in the image
     char_imgs = segment(line, word)
-    txt_word = ''
+    txt_word = []
     # For each character in the word
     for char_img in char_imgs:
         try:
@@ -39,8 +39,8 @@ def run2(obj):
             continue
         feature_vector = featurizer(ready_char)
         predicted_char = model.predict([feature_vector])[0]
-        txt_word += predicted_char
-    return txt_word
+        txt_word.append(predicted_char)
+    return ''.join(txt_word)
 
 
 def run(image_path):
@@ -59,15 +59,12 @@ def run(image_path):
     after = time.time()
 
     # append in the total string.
-    for word in predicted_words:
-        predicted_text += word
-        predicted_text += ' '
-
+    predicted_text = ' '.join(predicted_words)
     exc_time = after-before
     # Create file with the same name of the image
     img_name = image_path.split('/')[-1].split('.')[0]
 
-    with open(f'output/text/{img_name}.txt', 'w', encoding='utf8') as fo:
+    with open(f'pegon-result/text/{img_name}.txt', 'w', encoding='utf8') as fo:
         fo.writelines(predicted_text)
 
     return (img_name, exc_time)
@@ -90,8 +87,6 @@ if __name__ == "__main__":
         images_paths.extend(glob(f'{dataset_dir}/*.{t}'))
     before = time.time()
 
-    # pool = mp.Pool(mp.cpu_count())
-
     # # Method1
     # for image_path in images_paths:
     #     pool.apply_async(run,[image_path])
@@ -102,23 +97,30 @@ if __name__ == "__main__":
 
     running_time = []
     
-    for images_path in tqdm(images_paths,total=len(images_paths)):
-        img_name = images_path.split('/')[-1].split('.')[0].split(';')[0]
-        if img_name in tokens_to_unknown:
-            continue
-        try:
-            res = run(images_path)
-            running_time.append(res)
-        except:
-            continue
+    batch_size = 128
+    number_of_paths = len(images_paths)
+    
+    for i in range(0, number_of_paths, batch_size):
+        current_batch = images_paths[i:i+batch_size]
+        for images_path in tqdm(current_batch,total=len(current_batch)):
+            img_name_full = images_path.split('/')[-1].split('.')[0]
+            if os.path.exists(f'{result_dir}/text/{img_name_full}.txt'):
+                continue
+                
+            img_name = img_name_full.split(';')[0]
+            if img_name in tokens_to_unknown:
+                continue
+            try:
+                res = run(images_path)
+                running_time.append(res)
+            except:
+                continue
 
     running_time.sort()
     with open(f'{result_dir}/running_time.txt', 'w') as r:
         for t in running_time:
             r.writelines(f'image#{t[0]}: {t[1]}\n')       # if no need for printing 'image#id'.
-
-    # pool.close()
-    # pool.join()
+    
     after = time.time()
     print(f'total time to finish {len(running_time)} images: {after - before}')
     print(f'Successfully processing {len(running_time)} out of {len(images_paths)} images')
